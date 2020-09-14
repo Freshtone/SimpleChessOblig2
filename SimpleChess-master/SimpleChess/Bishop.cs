fb1d22421f95b519f20f44f67a342ead88516936
Jamie Kielland <jamie@freshfx.no>
2020-09-14 13:06:31 +0200

    finished

---
+++ SimpleChess-master/SimpleChess/Bishop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChess
+{
+    class Bishop : Piece
+    {
+        public Bishop() : base("Bishop"){}
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var ColoumMove = fromPosition[0] - toPosition[0];
+            var RowMove = fromPosition[1] - toPosition[1];
+
+            return Math.Abs(RowMove) == Math.Abs(ColoumMove);
+        }
+    }
+}
---
+++ SimpleChess-master/SimpleChess/Rook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChess
+{
+    class Rook : Piece
+    {
+        public Rook() : base("Rook"){}
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+    }
+}
